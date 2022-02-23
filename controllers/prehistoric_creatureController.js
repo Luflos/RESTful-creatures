9a5697fbf6b4df4329e6147b9a8a1221cf0dd498
Sol Youn <syoun93@gmail.com>
2022-02-22 17:46:38 -0800

    completed assignment

--- controllers/prehistoric_creatureController.js
+++ controllers/prehistoric_creatureController.js
@@ -8,6 +8,12 @@ const fs = require('fs')
 router.get('/prehistoric_creatures', (req, res) => {
   let creatures = fs.readFileSync('./prehistoric_creatures.json')
   let creatureData = JSON.parse(creatures)
+  let nameFilter = req.query.nameFilter
+  if(nameFilter) {
+    creatureData = creatureData.filter(creature => {
+      return creature.type.toLowerCase() === nameFilter.toLowerCase()
+    })
+  }
   res.render('prehistoric_creatures/prehistoricCreatures.ejs', {creatures: creatureData})
 })
 
@@ -20,12 +26,20 @@ router.get('/prehistoric_creatures/new', (req, res) => {
 router.get('/prehistoric_creatures/edit/:idx', (req, res) => {
   let creatures = fs.readFileSync('./prehistoric_creatures.json')
   let creatureData = JSON.parse(creatures)
+  let creatureIndex = req.params.idx
   let targetCreature = creatureData[creatureIndex]
   res.render('prehistoric_creatures/edit.ejs', {creature: targetCreature, creatureId: creatureIndex})
 })
 
 // PUT Route
-
+router.put('/prehistoric_creatures/:idx', (req, res) => {
+  let creatures = fs.readFileSync('./prehistoric_creatures.json')
+  let creatureData = JSON.parse(creatures)
+  creatureData[req.params.idx].type =req.body.type
+  creatureData[req.params.idx].img_url =req.body.img_url
+  fs.writeFileSync('./prehistoric_creatures.json', JSON.stringify(creatureData))
+  res.redirect('/prehistoric_creatures')
+})
 
 // SHOW single creature
 router.get('/prehistoric_creatures/:idx' , (req, res) => {
@@ -41,6 +55,16 @@ router.post('/prehistoric_creatures', (req, res) => {
   let creatures = fs.readFileSync('./prehistoric_creatures.json')
   let creatureData = JSON.parse(creatures)
   creatureData.push(req.body)
+  fs.writeFileSync('./prehistoric_creatures.json', JSON.stringify(creatureData))
+  res.redirect('/prehistoric_creatures')
+})
+
+// DELETE
+
+router.delete('/prehistoric_creatures/:idx', (req, res) => {
+  let creatures = fs.readFileSync('./prehistoric_creatures.json')
+  let creatureData = JSON.parse(creatures)
+  creatureData.splice(req.params.idx, 1)
   fs.writeFileSync('./prehistoric_creatures.json', JSON.stringify(creatureData))
   res.redirect('/prehistoric_creatures')
 })
